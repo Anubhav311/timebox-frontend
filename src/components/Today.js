fed9b1f60bb1e9774d447efa456a3ad3def79f81
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-03 21:23:37 +0530

    created structure of today page

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Div_today = styled.div`
+    width: 90%;
+    margin: auto;
+
+    .timeslot {
+        width: 90%;
+        margin: auto;
+        display: flex;
+    }
+
+    .slot-head {
+        background: red;
+        width: 20%;
+        padding: 5px;
+    }
+
+    .slot-body {
+        background: green;
+        width: 80%;
+        padding: 5px;
+    }
+`
+
 
 export default function Today(props) {
 
-    const timeboxMinutes = 10
+    const timeboxMinutes = 20
 
     let iterator = 1440
     let todayTimeSlots = []
@@ -24,20 +47,28 @@ export default function Today(props) {
         if (endHour == 24) {
             endHour = 0
         }
+
         todayTimeSlots.push(`${startHour}:${startMinute} - ${endHour}:${endMinute}`)
+
         startHour = endHour
         startMinute = startMinute + timeboxMinutes
+
         if (startMinute >= 60) {
             startMinute = 0
         }
     }
     return (
-        <div className="today">
-            {todayTimeSlots.map(num => (
-                <div>
-                    {num}
+        <Div_today className="today">
+            {todayTimeSlots.map((num, key) => (
+                <div className="timeslot">
+                    <div className="slot-head">
+                        <p>{num}</p>
+                    </div>
+                    <div className="slot-body">
+                        <p>do this {key}</p>
+                    </div>
                 </div>
             ))}
-        </div>
+        </Div_today>
     )
 }
