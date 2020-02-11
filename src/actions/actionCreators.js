17c7c485fc755d97284e3d7b77683121d404d079
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-11 22:04:01 +0530

    created startdate and enddate for filter data on backend

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,9 +1,14 @@
 import { GET_TASKS_REQUEST, GET_TASKS_SUCCESS, GET_TASKS_FAILURE } from './actionTypes'
 import axios from 'axios'
 
+let currentDate = new Date()
+let currentDay = currentDate.getDay()
+let startDate = new Date(); startDate.setDate(startDate.getDate() - currentDay + 1)
+let endDate = new Date(); endDate.setDate(endDate.getDate() - currentDay + 15)
 
+console.log(startDate, endDate)
 export const fetchTasks = () => dispatch => {
-    axios.get('http://localhost:4000/api/tasks')
+    axios.get(`http://localhost:4000/api/tasks?startdate=${startDate}`)
         .then(tasks => dispatch({
             type: GET_TASKS_REQUEST,
             payload: tasks.data
