2880a5be90e94ff282b3d660f9fb350e6a771f9c
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-12 04:41:34 +0530

    added dot to tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div_task = styled.div`
+    display: flex;
+    align-items: center;
+
+    .dot {
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background: yellow;
+        margin-right: 10px;
+    }
+`
 
 function Task(props) {
     return (
-        <div>
+        <Div_task>
+            <div className="dot"></div>
             <p>{props.task}</p>
-        </div>
+        </Div_task>
     )
 }
 
