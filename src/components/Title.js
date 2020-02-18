7ee0d4a350c8641596d533079e9c74be884ddc72
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-18 16:30:15 +0530

    deleted Text.js component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import InputField from './InputField';
-import Text from './Text';
 
 function Title(props) {
     const [isInEditMode, setIsInEditMode] = useState(false)
@@ -17,7 +16,7 @@ function Title(props) {
                 ? 
             <InputField taskIndex={props.taskId} changeEditMode={changeEditMode} isInEditMode={isInEditMode} text={props.text}/>
                 :
-            <Text text={props.text} changeEditMode={changeEditMode} className="partName" />}
+                <p onClick={changeEditMode}>{props.text}</p>}
         </div>
     )
 }
