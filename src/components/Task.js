77090878525c8711d3ee7bcb4ba8566017ee28ff
Anubhav311 <anubhavbhambri1@gmail.com>
2020-02-19 03:12:27 +0530

    implemented delete axios request

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 
 import Subtask from './Subtask';
 import Title from './Title';
+import Axios from 'axios';
+import { TaskContext } from './context/TasksContext';
 
 const Div_task = styled.div`
     .task-heading {
         display: flex;
         align-items: center;
+        justify-content: space-between;
     }
 
     .dot {
@@ -29,9 +32,29 @@ const Div_task = styled.div`
 
 function Task(props) {
     const [subtaskActive, setSubtaskActive] = useState('hide')
+    const {tasks, dispatch} = useContext(TaskContext)
+    const updatedTasksState = [...tasks]
 
     function toggleSubtask() {
         setSubtaskActive(subtaskActive === 'hide' ? 'subtasks' : 'hide')
+    }
+
+    function deleteTask() {
+        console.log(props.id, props.taskId)
+        updatedTasksState.splice(props.id, 1)
+        Axios.delete(`http://localhost:4000/api/tasks?task_id_pk=${props.taskId}`)
+            .then(res => {
+                if (props.taskId === res.data[0]) {
+                    console.log(res)
+                    dispatch({
+                        type: 'DELETE_TASK',
+                        payload: updatedTasksState
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
 
     return (
@@ -39,6 +62,7 @@ function Task(props) {
             <div className="task-heading">
                 <div className="dot" onClick={toggleSubtask}></div>
                 <Title taskId={props.id} text={props.task} />
+                <div style={{marginLeft: '10px', cursor: 'pointer'}} onClick={deleteTask}>x</div>
             </div>
             {subtaskActive === 'hide' ? '' : <Subtask subtaskActive={subtaskActive} taskId={props.taskId}/>}
         </Div_task>
