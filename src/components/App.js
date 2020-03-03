dbd0cd0d4886fbe54a0a388e273fb931d4633e17
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-04 00:58:14 +0530

    deleted Home.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import axios from 'axios';
 
-import Home from './Home';
-import TaskContextProvider from './context/TasksContext';
+import Today from './Today';
+import Tabs from './Tabs';
+import Week from './Week';
+import { thisWeek, nextWeek } from './DaysOfWeek';
+import { TaskContext } from './context/TasksContext';
+import { day1, day15 } from '../components/DaysOfWeek';
 
 function App() {
+  const {tasks, dispatch} = useContext(TaskContext)
+
+  useEffect(() => {
+      axios.get(`https://timebox-be.herokuapp.com/api/tasks?startdate=${day1.getFullYear()}-${('0' + (day1.getMonth() + 1)).slice(-2)}-${('0' + day1.getDate()).slice(-2)}T00:00:00Z&enddate=${day15.getFullYear()}-${('0' + (day15.getMonth() + 1)).slice(-2)}-${('0' + day15.getDate()).slice(-2)}T00:00:00Z`)
+          .then(tasks => dispatch({
+              type: 'GET_TASKS_REQUEST',
+              payload: tasks.data
+          }))
+  }, [])
+
   return (
-    <div className="">
-      <TaskContextProvider>
-        <Home/>
-      </TaskContextProvider>
-    </div>
+    <Tabs>
+      <div label="today">
+        <Today/>
+      </div>
+      <div label="this week">
+        <Week tasks={tasks} columnDate={thisWeek} />
+      </div>
+      <div label="next week">
+        <Week tasks={tasks} columnDate={nextWeek}/>
+      </div>
+    </Tabs>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import TaskContextProvider from './components/context/TasksContext';
 import App from './components/App';
 
 
 ReactDOM.render(
         <Router>
-            <App />
+            <TaskContextProvider>
+                <App />
+            </TaskContextProvider>
         </Router>
     , document.getElementById('root'));
 
