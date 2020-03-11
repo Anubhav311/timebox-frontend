95f1769d0475525624320d6a67041af437878b85
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-11 22:21:30 +0530

    added group title dynamically

--- src/components/DragAndDrop.js
+++ src/components/DragAndDrop.js
@@ -19,8 +19,9 @@ function DragAndDrop() {
             <div className="drag-n-drop">
                 {data.map((grp, grpI) => (
                     <div key={grp.title} className="dnd-group">
+                        <div className="group-title">{grp.title}</div>
                         {grp.items.map((item, itemI) => (
-                            <div key={item} className="dnd-item">
+                            <div draggable key={item} className="dnd-item">
                                 {item}
                             </div>
                         ))}
