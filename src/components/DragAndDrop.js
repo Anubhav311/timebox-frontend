19c3f6aae36a9cece4f42a0611cd34ab2f931ac1
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-11 21:37:42 +0530

    created boilerplate for drag and drop

---
+++ src/components/DragAndDrop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function DragAndDrop() {
+    return (
+        <div className="drag-n-drop">
+            <div className="dnd-group">
+                <div className="dnd-item">
+                    <div>
+                        <p>Item</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DragAndDrop;
