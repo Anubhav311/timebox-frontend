4b229aa7e301363d1b150877d89ba8bd581133da
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-11 22:26:54 +0530

    using state for components

--- src/components/DragAndDrop.js
+++ src/components/DragAndDrop.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './DragAndDrop.css';
 
 
 function DragAndDrop(props) {
+    const [list, setList] = useState(props.data)
     return (
         <div className="drag-n-drop">
-            {props.data.map((grp, grpI) => (
+            {list.map((grp, grpI) => (
                 <div key={grp.title} className="dnd-group">
                     <div className="group-title">{grp.title}</div>
                     {grp.items.map((item, itemI) => (
