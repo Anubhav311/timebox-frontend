e0118efb628993d348968fcc0f0cca9e5e76456b
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-15 17:43:35 +0530

    added some styles to time picker

--- src/components/view/TimePicker.js
+++ src/components/view/TimePicker.js
@@ -4,10 +4,22 @@ import './TimePicker.css';
 
 function TimePicker() {
     return (
-        <div>
-            <h1>Custom<spacn>Time</spacn>Pcker</h1>
+        <div className="time-picker-container" >
+            <h1>Custom<span>Time</span>Picker</h1>
             <div className="time-picker" dataTime="00:00">
-                <input type="number" name="" id="" />
+                <div className="hour">
+                    <div className="hour-up"></div>
+                    <input type="number" className="hr" value="00" />
+                    <div className="hour-down"></div>
+                </div>
+
+                <div className="separator">:</div>
+
+                <div className="minute">
+                    <div className="minute-up"></div>
+                    <input type="number" className="mn" value="00" />
+                    <div className="minute-down"></div>
+                </div>
             </div>
         </div>
     )
