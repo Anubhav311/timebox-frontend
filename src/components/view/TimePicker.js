b9a5b7562a0bb4b4a373490cd3025ef8a05cdcf4
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-17 00:57:11 +0530

    created event listners

--- src/components/view/TimePicker.js
+++ src/components/view/TimePicker.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
 import './TimePicker.css';
+import { HashRouter } from 'react-router-dom';
 
 function TimePicker() {
     const time_picker_element = document.querySelector('.time-picker');
 
     const hr_element = document.querySelector('.time-picker .hour .hr')
-    const hr_element = document.querySelector('.time-picker .minute .mn')
+    const min_element = document.querySelector('.time-picker .minute .mn')
 
-    const hr_element = document.querySelector('.time-picker .hour .hour-up')
-    const hr_element = document.querySelector('.time-picker .hour .hour-down')
+    const hr_up = document.querySelector('.time-picker .hour .hour-up')
+    const hr_down = document.querySelector('.time-picker .hour .hour-down')
 
     const min_up = document.querySelector('.time-picker .mn .minute-up')
-    const min_up = document.querySelector('.time-picker .mn .minute-down')
+    const min_down = document.querySelector('.time-picker .mn .minute-down')
+
+    hr_up.addEventListener('click', hour_up);
+    hr_down.addEventListener('click', hour_down);
+
+    min_up.addEventListener('click', minute_up);
+    min_down.addEventListener('click', minute_down);
 
 
     return (
