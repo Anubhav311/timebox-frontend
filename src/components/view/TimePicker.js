68fcedfbde31f48f0105e65466b56cb7b887326a
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-17 01:11:09 +0530

    defined event functions

--- src/components/view/TimePicker.js
+++ src/components/view/TimePicker.js
@@ -12,14 +12,95 @@ function TimePicker() {
     const hr_up = document.querySelector('.time-picker .hour .hour-up')
     const hr_down = document.querySelector('.time-picker .hour .hour-down')
 
-    const min_up = document.querySelector('.time-picker .mn .minute-up')
-    const min_down = document.querySelector('.time-picker .mn .minute-down')
+    const min_up = document.querySelector('.time-picker .minute .minute-up')
+    const min_down = document.querySelector('.time-picker .minute .minute-down')
+
+    let hour = 0
+    let minute = 0
 
     hr_up.addEventListener('click', hour_up);
     hr_down.addEventListener('click', hour_down);
 
     min_up.addEventListener('click', minute_up);
     min_down.addEventListener('click', minute_down);
+
+    hr_element.addEventListener('change', hour_change);
+    min_element.addEventListener('change', minute_change);
+
+    function hour_change(e) {
+        if (e.target.value > 23) {
+            e.target.value = 23;
+        } else if (e.target.value < 0) {
+            e.target.value = '00';
+        }
+
+        if (e.target.value ===  "") {
+            e.target.value = formatTime(hour);
+        }
+
+        hour = e.target.value
+    }
+
+    function minute_change(e) {
+        if (e.target.value > 59) {
+            e.target.value = 59;
+        } else if (e.target.value < 0) {
+            e.target.value = '00';
+        }
+
+        if (e.target.value ===  "") {
+            e.target.value = formatTime(minute);
+        }
+
+        minute = e.target.value
+    }
+
+    function hour_up() {
+        hour++;
+        if (hour > 23) {
+            hour = 0
+        }
+        setTime();
+    }
+
+    function hour_down() {
+        hour--;
+        if (hour < 0) {
+            hour = 23;
+        }
+        setTime();
+    }
+
+    function minute_up() {
+        minute++;
+        if (minute > 59) {
+            minute = 0;
+            hour++;
+        }
+        setTime();
+    }
+
+    function minute_down() {
+        minute--;
+        if (minute < 0) {
+            minute = 59;
+            hour--;
+        }
+        setTime();
+    }
+
+    function setTime() {
+        hr_element.value = formatTime(hour);
+        min_element.value = formatTime(minute);
+        time_picker_element.dataset.time = formatTime(hour) + ':' + formatTime(minute);
+    }
+
+    function formatTime(time) {
+        if (time < 10) {
+            time = '0' + time;
+        }
+        return time;
+    }
 
 
     return (
