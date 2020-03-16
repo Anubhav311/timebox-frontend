4c5754a4f6601dda99fbabaabb03b4a823b5e3b4
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-17 01:44:24 +0530

    updated hour_up function as per react

--- src/components/view/TimePicker.js
+++ src/components/view/TimePicker.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './TimePicker.css';
 import { HashRouter } from 'react-router-dom';
 
 function TimePicker() {
+    let d = new Date()
+
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    const [timeState, setTimeState] = useState({hour: hour, minute: minute})
+    setTime();
+
     const time_picker_element = document.querySelector('.time-picker');
 
     const hr_element = document.querySelector('.time-picker .hour .hr')
@@ -15,18 +22,17 @@ function TimePicker() {
     const min_up = document.querySelector('.time-picker .minute .minute-up')
     const min_down = document.querySelector('.time-picker .minute .minute-down')
 
-    let hour = 0
-    let minute = 0
 
-    hr_up.addEventListener('click', hour_up);
-    hr_down.addEventListener('click', hour_down);
+    // hr_up.addEventListener('click', hour_up);
+    // hr_down.addEventListener('click', hour_down);
 
-    min_up.addEventListener('click', minute_up);
-    min_down.addEventListener('click', minute_down);
+    // min_up.addEventListener('click', minute_up);
+    // min_down.addEventListener('click', minute_down);
 
-    hr_element.addEventListener('change', hour_change);
-    min_element.addEventListener('change', minute_change);
+    // hr_element.addEventListener('change', hour_change);
+    // min_element.addEventListener('change', minute_change);
 
+    // TODO if hour or minute is blank, it will input three zeros. fix that bug.
     function hour_change(e) {
         if (e.target.value > 23) {
             e.target.value = 23;
@@ -56,11 +62,10 @@ function TimePicker() {
     }
 
     function hour_up() {
-        hour++;
-        if (hour > 23) {
-            hour = 0
+        setTimeState({hour: timeState.hour + 1, minute: timeState.minute});
+        if (timeState.hour > 23) {
+            setTimeState({hour: 0, minute: timeState.minute})
         }
-        setTime();
     }
 
     function hour_down() {
@@ -90,9 +95,10 @@ function TimePicker() {
     }
 
     function setTime() {
-        hr_element.value = formatTime(hour);
-        min_element.value = formatTime(minute);
-        time_picker_element.dataset.time = formatTime(hour) + ':' + formatTime(minute);
+        // setTimeState({hour: formatTime(timeState.hour), minute: formatTime(timeState.minute)})
+        // hr_element.value = formatTime(timeState.hour);
+        // min_element.value = formatTime(timeState.minute);
+        // time_picker_element.dataset.time = formatTime(timeState.hour) + ':' + formatTime(timeState.minute);
     }
 
     function formatTime(time) {
@@ -108,17 +114,17 @@ function TimePicker() {
             <h1>Custom<span>Time</span>Picker</h1>
             <div className="time-picker" dataTime="00:00">
                 <div className="hour">
-                    <div className="hour-up"></div>
-                    <input type="number" className="hr" value="00" />
-                    <div className="hour-down"></div>
+                    <div className="hour-up" onClick={hour_up} ></div>
+                    <input type="number" className="hr" value={timeState.hour} onChange={hour_change} />
+                    <div className="hour-down" onClick={hour_down} ></div>
                 </div>
 
                 <div className="separator">:</div>
 
                 <div className="minute">
-                    <div className="minute-up"></div>
-                    <input type="number" className="mn" value="00" />
-                    <div className="minute-down"></div>
+                    <div className="minute-up" onClick={minute_up} ></div>
+                    <input type="number" className="mn" value={timeState.minute} onChange={minute_change} />
+                    <div className="minute-down" onClick={minute_down} ></div>
                 </div>
             </div>
         </div>
