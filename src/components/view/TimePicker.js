a29eb7e115bfde230abb9185268b8300a4f1cc05
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-17 00:53:31 +0530

    created variables for events

--- src/components/view/TimePicker.js
+++ src/components/view/TimePicker.js
@@ -3,6 +3,18 @@ import React from 'react';
 import './TimePicker.css';
 
 function TimePicker() {
+    const time_picker_element = document.querySelector('.time-picker');
+
+    const hr_element = document.querySelector('.time-picker .hour .hr')
+    const hr_element = document.querySelector('.time-picker .minute .mn')
+
+    const hr_element = document.querySelector('.time-picker .hour .hour-up')
+    const hr_element = document.querySelector('.time-picker .hour .hour-down')
+
+    const min_up = document.querySelector('.time-picker .mn .minute-up')
+    const min_up = document.querySelector('.time-picker .mn .minute-down')
+
+
     return (
         <div className="time-picker-container" >
             <h1>Custom<span>Time</span>Picker</h1>
