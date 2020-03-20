908176e6bd288cef16be6b9843e4f687c29ae8ee
Anubhav311 <anubhavbhambri1@gmail.com>
2020-03-20 18:16:33 +0530

    did some code cleaning

--- src/components/view/Modal.js
+++ src/components/view/Modal.js
@@ -4,21 +4,17 @@ import ReactDOM from 'react-dom';
 import TimePicker from './TimePicker';
 import './Modal.css';
 
-const Modal = ({ isShowing, hide, taskIndex, columnIndex, taskIdPk }) => isShowing ? ReactDOM.createPortal(
+const Modal = (props) => props.isShowing ? ReactDOM.createPortal(
     <React.Fragment>
         <div className="modal-overlay"/>
         <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
         <div className="modal">
             <div className="modal-header">
-            <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+            <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={props.hide}>
                 <span aria-hidden="true">&times;</span>
             </button>
             </div>
-            <TimePicker
-                taskIndex={taskIndex}
-                columnIndex={columnIndex}
-                taskIdPk={taskIdPk}
-            />
+            {props.children}
         </div>
         </div>
     </React.Fragment>, document.body
