c1eaea67f629fb888af3f72f14dbc649c9178b07
Anubhav311 <anubhavbhambri1@gmail.com>
2020-04-18 13:55:02 +0530

    added styles to 404 page

--- src/components/view/NoMatchPage.js
+++ src/components/view/NoMatchPage.js
@@ -5,15 +5,11 @@ import '../../styles/NoMatchPage.css';
 function NoMatch({location}) {
     return (
         <div className="nomatch_container">
-            {/* <div className="top_row">
-                <h3>Oooops!</h3>
-                <p>The page you are looking for doesn't exist</p>
-            </div> */}
-            <div className="mid_row">
+            <div className="text_404">
                 <h1>404</h1>
                 <p>ERROR 404: PAGE NOT FOUND</p>
             </div>
-            <div className="bottom_row">
+            <div className="reroute">
                 <button>Going back to Today</button>
             </div>
         </div>
