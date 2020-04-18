09847726e487fe5b00486fae36e0db6bd7ea6c67
Anubhav311 <anubhavbhambri1@gmail.com>
2020-04-18 13:02:33 +0530

    added more text to NoMatch page

--- src/components/view/NoMatchPage.js
+++ src/components/view/NoMatchPage.js
@@ -3,7 +3,11 @@ import React from 'react';
 function NoMatch({location}) {
     return (
         <div>
-            <h3>No match for <code>{location.pathname}</code></h3>
+            <h1>404</h1>
+            <div>
+                <h3>It's dark out here</h3>
+                <h5>The page you are looking for doesn't seem to exist</h5>
+            </div>
         </div>
     )
 }
