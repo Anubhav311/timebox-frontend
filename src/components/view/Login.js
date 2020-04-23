08ebe613300a2f5001c25981bdfcfef0a99ca25c
Anubhav311 <anubhavbhambri1@gmail.com>
2020-04-23 21:14:49 +0530

    changed layout of login form

--- src/components/view/Login.js
+++ src/components/view/Login.js
@@ -1,5 +1,7 @@
 import React, { useCallback, useContext } from 'react';
 import { withRouter, Redirect } from 'react-router';
+
+import '../../styles/Login.css';
 import app from './FBase';
 import { AuthContext } from './Auth.js';
 
@@ -24,18 +26,18 @@ const Login = ({ history }) => {
     }
 
     return (
-        <div>
+        <div className="login_container">
             <h1>Log in</h1>
-            <form onSubmit={handleLogin}>
-                <label>
-                    Email
+            <form onSubmit={handleLogin} className="login_form">
+                <label className="row_1">
+                    <p>Email</p>
                     <input name="email" type="email" placeholder="Email" />
                 </label>
-                <label>
-                    Password
+                <label className="row_2">
+                    <p>Password</p>
                     <input name="password" type="password" placeholder="Placeholder" />
                 </label>
-                <button type="submit">Log in</button>
+                <button className="login_button" type="submit">Log in</button>
             </form>
         </div>
     )
