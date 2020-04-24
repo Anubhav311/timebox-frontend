91c72b819cecf589f720de1e04c7e4feef4e21c5
Anubhav311 <anubhavbhambri1@gmail.com>
2020-04-24 21:21:24 +0530

    inserted navbar in settings page

--- src/components/view/Settings.js
+++ src/components/view/Settings.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import app from './FBase';
+import NavBar from './NavBar';
 
 function Settings() {
     return (
         <>
+            <NavBar />
             <div>dayummm!!!</div>
             <button onClick={() => app.auth().signOut()}>Sign Out</button>
         </>
