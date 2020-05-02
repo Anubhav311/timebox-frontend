87fcfc24bfb19c1d4c129a0bfd76e4be99f6c3ee
Anubhav311 <anubhavbhambri1@gmail.com>
2020-05-02 11:09:30 +0530

    changed structure of signup form

--- src/components/view/SignUp.js
+++ src/components/view/SignUp.js
@@ -6,32 +6,32 @@ import '../../styles/SignUp.css';
 import app from '../auth/FBase';
 
 const SignUp = ({ history }) => {
-    // const handleSingUp = useCallback(async event => {
-    //     event.preventDefault()
-    //     const { email, password } = event.target.elements;
-    //     try {
-    //         await app
-    //             .auth()
-    //             .createUserWithEmailAndPassword(email.value, password.value);
-    //         history.push("/");
-    //     } catch (error) {
-    //         alert(error);
-    //     }
-    // }, [history]);
-    function handleSingUp(event) {
+    const handleSingUp = useCallback(async event => {
         event.preventDefault()
-        const { email, password, name, timebox } = event.target.elements;
-        const payload = {
-            email: email.value,
-            password: password.value,
-            name: name.value,
-            timebox: timebox.value
+        const { email, password } = event.target.elements;
+        try {
+            await app
+                .auth()
+                .createUserWithEmailAndPassword(email.value, password.value);
+            history.push("/");
+        } catch (error) {
+            alert(error);
         }
-        console.log(payload)
+    }, [history]);
+    // function handleSingUp(event) {
+    //     event.preventDefault()
+    //     const { email, password, name, timebox } = event.target.elements;
+    //     const payload = {
+    //         email: email.value,
+    //         password: password.value,
+    //         name: name.value,
+    //         timebox: timebox.value
+    //     }
+    //     console.log(payload)
 
-        return axios.post('http://localhost:4000/api/users', payload)
-            .then(res => console.log(res.data))
-    }
+    //     return axios.post('http://localhost:4000/api/users', payload)
+    //         .then(res => console.log(res.data))
+    // }
 
     return (
         <div>
@@ -44,14 +44,6 @@ const SignUp = ({ history }) => {
                 <label className="signup_row_2">
                     <p>Password</p>
                     <input name="password" type="password" placeholder="password" />
-                </label>
-                <label className="signup_row_3">
-                    <p>Name</p>
-                    <input name="name" type="name" placeholder="name" />
-                </label>
-                <label className="signup_row_4">
-                    <p>Timebox</p>
-                    <input name="timebox" type="number" placeholder="timebox" />
                 </label>
                 <button className="signup_button" type="submit">Sign Up</button>
             </form>
